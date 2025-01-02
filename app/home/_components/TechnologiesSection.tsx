d9d72e691d6695a3c134b117ecd5480faa0c0f09
SagarFyntune <sagar.chakkarkota@fyntune.com>
2025-01-03 00:16:24 +0530

    enhance: added border for logo

--- app/home/_components/TechnologiesSection.tsx
+++ app/home/_components/TechnologiesSection.tsx
@@ -12,7 +12,7 @@ const TechnologiesSection = () => {
       className="p-2 md:p-4 rounded-lg shadow-blue/20 shadow-lg border border-blue/40 flex flex-col items-center justify-center gap-8 max-sm:gap-2"
     >
       <h1 className="text-3xl max-sm:text-xl">Technologies</h1>
-      <div className="max-sm:max-w-[calc(100vw-20px)] flex gap-5 max-sm:overflow-auto pb-3">
+      <div className="max-sm:max-w-[calc(100vw-20px)] flex gap-5 max-sm:overflow-auto pb-4">
         {logoArray.map((logo) => (
           <button
             key={logo.alt}
